1f3387341cb530b756e0d773909e849a3afe5e1d
Ou Liu <oliu@finivation.com>
2018-05-08 10:12:42 -0400

    May 8

---
+++ Week3/693.cs
@@ -0,0 +1,17 @@
+// 100% - but can be improved I think
+public class Solution {
+    public bool HasAlternatingBits(int n) {            
+        bool isOne = (n % 2 == 0);
+        while(n != 0){
+            n >>= 1;
+            if (isOne){
+                if (n % 2 == 0) return false;
+            } 
+            else {
+                if (n % 2 == 1) return false;
+            }
+            isOne = !isOne;            
+        }
+        return true;
+    }
+}
