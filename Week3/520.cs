c91cf646a5a63d313bc8cff1d3e93ab35f18e372
Liu <ou.liu@cunamutual.com>
2018-05-13 17:27:20 -0400

    May 13

---
+++ Week3/520.cs
@@ -0,0 +1,16 @@
+// 95.83%
+public class Solution {
+    public bool DetectCapitalUse(string word) {
+        if(word.Length <= 1)
+            return true;
+        bool isFirstCapital = char.IsUpper(word[0]);
+        bool isCapital = char.IsUpper(word[1]);
+        if(!isFirstCapital && isCapital)
+            return false;
+        for(int i = 2; i < word.Length; ++i) {
+            if(char.IsUpper(word[i]) != isCapital)
+                return false;
+        }
+        return true;
+    }
+}
